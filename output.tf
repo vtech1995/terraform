a4733eb58c44f1a51afe33796820bce3466cb9ae
vtech1995 <ajangra95@gmail.com>
2022-06-28 19:45:29 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,13 @@
+output "master" {
+   value = aws_instance.ec2.public_ip
+}
+
+#output "Worker1" {
+#   value = aws_instance.ec2_worker1.public_ip
+#}
+
+
+# output "Worker2" {
+#    value = aws_instance.ec2_worker2.public_ip
+# }
+
