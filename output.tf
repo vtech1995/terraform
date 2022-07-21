834f4a5bbaabd086801936b6a5fe4b3612efcd50
vtech1995 <ajangra95@gmail.com>
2022-07-21 19:48:11 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,15 @@
-output "master" {
-   value = aws_instance.ec2.public_ip
+# output "master" {
+#   value = aws_instance.ec2.public_ip
+# }
+
+output "eip"{
+   value = aws_eip.master_eip.public_ip
 }
 
-#output "Worker1" {
+# output "Worker1" {
 #   value = aws_instance.ec2_worker1.public_ip
-#}
-
+# }
 
 # output "Worker2" {
 #    value = aws_instance.ec2_worker2.public_ip
 # }
-
