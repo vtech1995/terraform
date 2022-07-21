a0a2099c5e5f72ffdc7d659ea7c6d669df09110a
vtech1995 <ajangra95@gmail.com>
2022-07-21 19:35:07 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,27 @@
    instance_type = var.instancetype
    subnet_id = aws_subnet.public_subnet.1.id
    vpc_security_group_ids = ["${aws_security_group.ec2-sg.id}"]
-   associate_public_ip_address = "true"
+   associate_public_ip_address = "flase"
    key_name  = var.key_name
-
+   user_data = <<EOF
+    #!/bin/bash
+    sudo yum update –y
+    sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+    sudo yum upgrade
+    sudo amazon-linux-extras install java-openjdk11 -y
+    sudo yum install jenkins -y
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
+    sudo cat /etc/sudoers.d/90-cloud-init-users >> /tmp/90-cloud-init-users
+    sudo echo -e "jenkins ALL=(ALL) NOPASSWD:ALL"  >> /tmp/90-cloud-init-users
+    sudo cp  /tmp/90-cloud-init-users /etc/sudoers.d/90-cloud-init-users    
+    sudo yum install -y yum-utils   
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+    sudo yum -y install terraform
+    sudo cat /var/lib/jenkins/secrets/initialAdminPassword >> /home/ec2-user/Jpassword.txt
+   EOF
+   
    
    tags = {
      Name = "Master-${var.region}"
